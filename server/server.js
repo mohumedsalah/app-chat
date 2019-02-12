c5eec0fb334f55dfec097ca7586bc4138a707df2
Akram <akram.sameer99@gmail.com>
2019-02-12 16:34:20 +0200

    prepare to deploy

--- server/server.js
+++ server/server.js
@@ -7,8 +7,13 @@ var bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
+const url =
+  process.env.NODE_ENV === 'production'
+    ? 'mongodb://admin:admin123@ds131765.mlab.com:31765/chat-app-2'
+    : 'mongodb://localhost:27017/chatApp';
+
 mongoose
-  .connect('mongodb://localhost:27017/chatApp')
+  .connect(url)
   .then(() => console.log('connected success to db'))
   .catch(() => console.error('there is error to connect with db'));
 
@@ -20,33 +25,29 @@ app.use(helmet());
 app.use('/api/user', user.router);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../clinet/build'));
+  app.use(express.static('../client/build'));
 
   app.get('*', (req, res) => {
-    const pth = path.resolve(__dirname, '..', 'clinet', 'build', 'index.html');
-    console.log(pth);
-
+    const pth = path.resolve(__dirname, '..', 'client', 'build', 'index.html');
     res.sendFile(pth);
   });
 }
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const Users = [];
-io.on('connection', function (socket) {
-   
-  socket.on('current', (user) => {
-    const users = Users.filter(c=> c.id !== user._id)
-    socket.emit('hello', users );
+io.on('connection', function(socket) {
+  socket.on('current', user => {
+    const users = Users.filter(c => c.id !== user._id);
+    socket.emit('hello', users);
     const user_ = Users.find(c => c.id === user._id);
-    if(user_){
+    if (user_) {
       return;
     }
-    Users.push({ id: user._id, socketId: socket.id, name: user.name });   
+    Users.push({ id: user._id, socketId: socket.id, name: user.name });
     socket.broadcast.emit('newOneComming', user);
   });
 
-
-  socket.on('sendMessage', (obj) => {
+  socket.on('sendMessage', obj => {
     var result = Users.find(el => el.id === obj.to);
     var sendId = Users.find(el => el.socketId === socket.id);
     if (!result) {
@@ -54,21 +55,16 @@ io.on('connection', function (socket) {
     }
     const messageSend = { content: obj.content, from: sendId.id };
     io.to(result.socketId).emit('newMessageComming', messageSend);
-  })
-
-
+  });
 
   socket.on('disconnect', () => {
-    try{
-    var index = Users.findIndex(item => item.socketId === socket.id)
-    io.sockets.emit('disconnectOne',Users[index].id);
-    Users.splice(index, 1);
-    }catch(e){
-    }
-  })
-  
+    try {
+      var index = Users.findIndex(item => item.socketId === socket.id);
+      io.sockets.emit('disconnectOne', Users[index].id);
+      Users.splice(index, 1);
+    } catch (e) {}
+  });
 });
-
 
 server.listen(process.env.PORT || '8000', () =>
   console.log('sever lisent to port : 8000')
